4604bd7b1401b5b8ef4dd16c5a81ae6a5b2e7e85
nsjteo <n.teomacdonald@gmail.com>
2022-04-27 21:28:09 -0700

    Create redux store, index.ts for exposed redux functionality

---
+++ src/redux/reducers/index.ts
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import repositoriesReducer from './repositoriesReducer';
+
+const rootReducer = combineReducers({ repositories: repositoriesReducer });
+
+export default rootReducer;
